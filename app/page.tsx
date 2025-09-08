03d1beb624002054d88bd807615bbf40ebf22c3c
Hiroshi Murata <31mh2019@gmail.com>
2025-09-08 13:00:25 +0900

    サイトの名前を変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div>
-      ホノルルマラソンを走る会の大会結果ページです
+      マラソンやトラックレースの記録サイト
       <p><a href="./result">結果</a></p>
-      <p><a href="./competition">参加大会</a></p>
+      <p><a href="./competition">大会一覧</a></p>
     </div>
   );
 }
