ac5a998a1d2f5c7b761b265b6e578f85059f510e
Hiroshi Murata <31mh2019@gmail.com>
2025-10-19 16:28:54 +0900

    タイトル変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "マラソン記録ツール developed by H.Murata",
+  title: "Runner’s Record",
   description: "",
 };
 
